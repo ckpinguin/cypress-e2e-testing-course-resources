7c9b671e1be175873e7997caec1c324b61f2a3f1
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-28 17:58:26 +0200

    section finished

--- code/05 Stubs, Spies/01 Starting Project/cypress/e2e/location.cy.js
+++ code/05 Stubs, Spies/01 Starting Project/cypress/e2e/location.cy.js
@@ -2,6 +2,7 @@
 
 describe("share location", () => {
   beforeEach(() => {
+    cy.clock()
     cy.fixture("user-location.json").as("userLocation")
     cy.visit("/").then((win) => {
       cy.get("@userLocation").then((fakePosition) => {
@@ -48,6 +49,7 @@ describe("share location", () => {
     cy.get('[data-cy="share-loc-btn"]').click()
     cy.get("@getStoredLocation").should("have.been.called")
     cy.get('[data-cy="info-message"]').should("be.visible")
+    cy.tick(2000)
     cy.get('[data-cy="info-message"]').should("not.be.visible")
   })
 })
